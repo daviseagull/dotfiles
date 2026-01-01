dc6bbe71b128ae28299dc5846953836cb4f00d5e
Davi Seagull <daviseagull@icloud.com>
2026-01-01 18:03:30 -0500

    chore: remove any neovim/tmux config files

--- nvim-bkp/lua/plugins/mini.lua
+++
@@ -1,3 +0,0 @@
-require('mini.icons').mock_nvim_web_devicons()
-require('mini.ai').setup({})
-require('mini.surround').setup({})
--- nvim-bkp/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-require("trouble").setup({
-  icons = true,
-  use_diagnostic_signs = true,
-  modes = {
-    lsp = {
-      win = { position = "right" },
-    },
-  },
-})
